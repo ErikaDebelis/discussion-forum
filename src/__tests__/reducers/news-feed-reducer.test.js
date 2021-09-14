63c1ceae052642790acbfad3c0d78ebe2a0b770e
Erika Debelis <erika.debelis@gmail.com>
2021-09-14 14:55:29 -0700

    write 3rd test; delete post- good fail
    Co-authored-by: Godfrey Owidi <godfreyowiidi@gmail.com>
    Co-authored-by: Araceli Valdovinos <valdovinosaraceli50@gmail.com>

--- src/__tests__/reducers/news-feed-reducer.test.js
+++ src/__tests__/reducers/news-feed-reducer.test.js
@@ -3,12 +3,25 @@ import newsFeedReducer from '../../reducers/news-feed-reducer';
 describe('newsFeedReducer', () => {
 
   let action;
+
+  const currentState = {
+    1: {name: 'Godfrey',
+    location: 'Kenya',
+    content: 'Opioids are poison!',
+    id: 1 },
+    2: {name: 'Araceli',
+    location: 'Texas',
+    content: 'Texans love their guns.',
+    id: 2 }
+  };
+
   const postData = {
     name: 'Godfrey',
     location: 'Kenya',
     content: 'Opioids are poison!',
     id: 1
   };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(newsFeedReducer({}, { type: null })).toEqual({});
   });
@@ -30,6 +43,20 @@ describe('newsFeedReducer', () => {
         content: content,
         id: id
       }
+    });
+  });
+
+  test('Should successfully delete a ticket', () => {
+    action = {
+      type: 'DELETE_TICKET',
+      id: 1
+    };
+
+    expect(newsFeedReducer(currentState, action)).toEqual({
+      2: {name: 'Araceli',
+        location: 'Texas',
+        content: 'Texans love their guns.',
+        id: 2 }
     });
   });
 });
