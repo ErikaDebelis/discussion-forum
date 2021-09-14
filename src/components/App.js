7a970632dfd7f059b3ecda7c329172cb4f2a464c
Erika Debelis <erika.debelis@gmail.com>
2021-09-14 15:08:49 -0700

    begin logic in postcontrol
    Co-authored-by: Godfrey Owidi <godfreyowiidi@gmail.com>
    Co-authored-by: Araceli Valdovinos <valdovinosaraceli50@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <p>FORUM</p>
       </header>
+      <PostControl />
     </div>
   );
 }
--- src/components/PostControl.js
+++ src/components/PostControl.js
@@ -1,2 +1,33 @@
 import React from "react";
-import 
+import NewsFeed from "./NewsFeed"; 
+import NewPostFrom from "./NewPostForm";
+
+class PostControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    }
+  }
+
+
+  render(){
+  let currentlyVisibleState = null;
+  if (this.state.formVisibleOnPage){
+    currentlyVisibleState = <NewPostForm />
+  }else {
+    currentlyVisibleState = <NewsFeed /> 
+    addPostButton = <button onClick={this.handleClick}>Add Post</button>
+  }
+    return(
+      <React.Fragment>
+        {currentlyVisibleState}
+        {addPostButton}
+      </React.Fragment>
+    )
+  }
+}
+
+export default PostControl;
+
+
